c302feb78cee6d42b271aa00ea415ea31506e6f9
Kevin R. Sherman <kevin.r.sherman@gmail.com>
2024-06-25 17:33:42 -0400

    working on interceptors

--- src/trader/interceptors.ts
+++ src/trader/interceptors.ts
@@ -1,44 +1,29 @@
 import axios from "axios";
-// const get = require("lodash/get");
+import Trader from "./trader";
+import Base from "./base";
+import get from "lodash.get";
 
-/**
- * @typedef {import('./base')} Base
- */
-/**
- * Append access token to the request.
- *
- * @private
- * @param {Base} client Client
- * @returns {void}
- */
-// function appendAccessToken(client) {
-//   client.axios.interceptors.request.use((request) => {
-//     if (client.config.accessToken) {
-//       request.headers.Authorization = `Bearer ${client.config.accessToken}`;
-//     }
+function appendAccessToken(trader: Trader) {
+  trader.axios.interceptors.request.use((request: any) => {
+    if (trader.config.accessToken) {
+      request.headers.Authorization = `Bearer ${trader.config.accessToken}`;
+    }
 
-//     return request;
-//   });
-// } // appendAccessToken()
+    return request;
+  });
+}
 
-/**
- * Update config when a new token is received.
- *
- * @private
- * @param {Base} client Client
- * @returns {void}
- */
-// function updateConfigOnNewToken(client) {
-//   client.axios.interceptors.response.use((response) => {
-//     if (response.config.url === "/oauth2/token") {
-//       const token = parseToken(response.data);
-//       Object.assign(client.config, token);
-//       client._emitter.emit("token", token);
-//     }
+function updateConfigOnNewToken(trader: Trader) {
+  trader.axios.interceptors.response.use((response: any) => {
+    if (response.config.url === "/oauth2/token") {
+      const token = parseToken(response.data);
+      Object.assign(trader.config, token);
+      trader._emitter.emit("token", token);
+    }
 
-//     return response;
-//   });
-// } // updateConfigOnNewToken()
+    return response;
+  });
+}
 
 /**
  * Refresh token and retry request on status code 401.
@@ -47,128 +32,99 @@ import axios from "axios";
  * @param {Base} client Client
  * @returns {void}
  */
-// function refreshAndRetry(client) {
-//   client.axios.interceptors.response.use(undefined, (error) => {
-//     const originalRequest = error.config;
-//     const condition =
-//       client.config.refreshAndRetry &&
-//       get(error, "response.status") === 401 &&
-//       !originalRequest._retry &&
-//       originalRequest.url !== "/oauth2/token";
+function refreshAndRetry(trader: Trader) {
+  trader.axios.interceptors.response.use(undefined, (error: any) => {
+    const originalRequest = error.config;
+    const condition =
+      trader.config.refreshAndRetry &&
+      get(error, "response.status") === 401 &&
+      !originalRequest._retry &&
+      originalRequest.url !== "/oauth2/token";
 
-//     if (condition) {
-//       originalRequest._retry = true;
-//       return client.refreshAccessToken().then(() => {
-//         originalRequest.headers.Authorization = `Bearer ${client.config.accessToken}`;
-//         return axios(originalRequest);
-//       });
-//     }
+    if (condition) {
+      originalRequest._retry = true;
+      return trader.refreshAccessToken(trader.config.accessToken).then(() => {
+        originalRequest.headers.Authorization = `Bearer ${trader.config.accessToken}`;
+        return axios(originalRequest);
+      });
+    }
 
-//     return Promise.reject(error);
-//   });
-// } // refreshAndRetry()
+    return Promise.reject(error);
+  });
+}
 
-/**
- * Return the full Axios response.
- *
- * @private
- * @param {Base} client Client
- * @returns {void}
- */
-// function fullResponse(client) {
-//   client.axios.interceptors.response.use(
-//     (response) => {
-//       return client.config.returnFullResponse
-//         ? response
-//         : get(response, "data");
-//     },
-//     (error) => {
-//       // we don't want to Promise.reject() as it will
-//       // trigger an ERR_UNHANDLED_REJECTION error.
+function fullResponse(trader: Trader) {
+  trader.axios.interceptors.response.use(
+    (response: any) => {
+      return trader.config.returnFullResponse
+        ? response
+        : get(response, "data");
+    },
+    (error: any) => {
+      // we don't want to Promise.reject() as it will
+      // trigger an ERR_UNHANDLED_REJECTION error.
 
-//       // axios response
-//       if (client.config.returnFullResponse) {
-//         throw error;
-//       }
+      // axios response
+      if (trader.config.returnFullResponse) {
+        throw error;
+      }
 
-//       // custom error response
-//       if (error.response) {
-//         const customError = {
-//           status: get(error, "response.status"),
-//           error: get(error, "response.data.error"),
-//         };
+      // custom error response
+      if (error.response) {
+        const customError = {
+          status: get(error, "response.status"),
+          error: get(error, "response.data.error"),
+        };
 
-//         throw customError;
-//       }
+        throw customError;
+      }
 
-//       // extract error
-//       const customError = new Error(error.message);
-//       customError.stack = error.stack;
-//       throw customError;
-//     }
-//   );
-// } // fullResponse()
+      // extract error
+      const customError = new Error(error.message);
+      customError.stack = error.stack;
+      throw customError;
+    }
+  );
+}
 
 //#region helpers
 
-/**
- * Transform the given token object
- *
- * @private
- * @param {object} data Token info return from oauth endpoint
- * @returns {object} Transformed token object
- */
-// function parseToken(data) {
-//   const res = {
-//     accessToken: data.access_token,
-//     accessTokenExpiresAt: timeFromNow(data.expires_in),
-//     refreshToken: data.refresh_token,
-//     refreshTokenExpiresAt: timeFromNow(data.refresh_token_expires_in),
-//     scope: data.scope,
-//     tokenType: data.token_type,
-//   };
+function parseToken(data: any) {
+  const res = {
+    accessToken: data.access_token,
+    accessTokenExpiresAt: timeFromNow(data.expires_in),
+    refreshToken: data.refresh_token,
+    refreshTokenExpiresAt: timeFromNow(data.refresh_token_expires_in),
+    scope: data.scope,
+    tokenType: data.token_type,
+  };
 
-//   // remove props with falsey values
-//   return filterObj(res, (value) => value);
-// } // parseToken()
+  // remove props with falsey values
+  return filterObj(res, (value: any) => value);
+} // parseToken()
 
-/**
- * Get the UTC time from now
- *
- * @private
- * @param {number} seconds Number of seconds
- * @returns {string|undefined} UTC time string or undefined
- */
-// function timeFromNow(seconds) {
-//   return seconds
-//     ? new Date(Date.now() + 1000 * seconds).toISOString()
-//     : undefined;
-// } // getTimeFromNow()
+function timeFromNow(seconds: number) {
+  return seconds
+    ? new Date(Date.now() + 1000 * seconds).toISOString()
+    : undefined;
+}
 
-/**
- * Filter object
- *
- * @private
- * @param {object} obj Object to filter
- * @param {Function} cb Callback
- * @returns {object} Filtered object
- */
-// function filterObj(obj, cb) {
-//   return Object.keys(obj).reduce((acc, cur) => {
-//     if (cb(obj[cur], cur)) {
-//       acc[cur] = obj[cur];
-//     }
-//     return acc;
-//   }, {});
-// } // filterObj()
+function filterObj(obj: any, cb: any) {
+  return Object.keys(obj).reduce((acc: any, cur: any) => {
+    if (cb(obj[cur], cur)) {
+      acc[cur] = obj[cur];
+    }
+    return acc;
+  }, {});
+}
 
 //#endregion
 
 const interceptors = {
-  // appendAccessToken,
-  // updateConfigOnNewToken,
-  // refreshAndRetry,
-  // fullResponse,
+  appendAccessToken,
+  updateConfigOnNewToken,
+  refreshAndRetry,
+  fullResponse,
 };
 
 /**
@@ -178,8 +134,8 @@ const interceptors = {
  * @param {Base} client Client
  * @returns {void}
  */
-function setup(trader: any) {
-  //Object.keys(interceptors).forEach((key) => interceptors[key](trader));
+function setup(base: Base) {
+  Object.keys(interceptors).forEach((key: string) => interceptors[key](base));
 }
 
 export default Object.assign({}, interceptors, { setup });
